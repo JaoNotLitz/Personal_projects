03b3fd3efa5adbf6c58b1e388a66688b8523330a
João Gabriel <128253740+JaoNotLitz@users.noreply.github.com>
2024-02-19 11:32:40 -0300

    Add files via upload

---
+++ typing.java
@@ -0,0 +1,37 @@
+
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Thread;
+
+public class helloworld {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        char randomChar;
+        System.out.println("digite a palavra");
+        String palavra = sc.nextLine();
+
+        String[] array = palavra.split("");
+        String formacao = "";
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.println(palavra);
+            } else{
+            if (array[i].charAt(0) == ' ') {
+                formacao += ' ';
+            } else {
+                do {
+                    randomChar = (char) ('a' + Math.abs(random.nextInt()) % 26);
+
+                    if (randomChar == array[i].charAt(0)) {
+                        formacao += randomChar;
+                    }
+                    System.out.println(formacao + randomChar);
+                    Thread.sleep(10);
+                } while (randomChar != array[i].charAt(0));
+            }
+        }
+        sc.close();
+    }
+}
+}
